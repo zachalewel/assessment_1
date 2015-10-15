213110596ca06c5f0a0cd685047117a4d5f20623
Zach Alewel <zachalewel@gmail.com>
2015-10-15 17:01:42 -0600

    Adds payment method [#105732218]

--- credit_card.rb
+++ credit_card.rb
@@ -1,11 +1,16 @@
+#!/usr/bin/env ruby
 # hey rubocop
 class CreditCard
-  attr_reader :balance
+  attr_reader :balance, :payment
   def initialize
     @balance = 0.00
   end
 
-  def charge(ammount)
-    @balance += ammount
+  def charge(amount)
+    @balance += amount
+  end
+
+  def payment(amount)
+    @balance -= amount
   end
 end
